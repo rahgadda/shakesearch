94a8fa41d0509bdb7e6bf823f6435a991cc69a36
rahgadda <gaddam.rahul@gmail.com>
2021-03-16 17:36:44 +0000

    Initial Draft

--- components/Body.jsx
+++ components/Body.jsx
@@ -2,11 +2,11 @@ function Body(){
     return (
         <Grid container direction="column" spacing={2}>
             <Grid item xs={1} /> 
-            <Grid item xs={2} container style={{"max-width": "100%"}}>
+            <Grid item xs={2} container style={{"width": "100%"}}>
                 Search Box
             </Grid> 
             <Grid item xs={2} /> 
-            <Grid item xs={6} container style={{"max-width": "100%"}}>
+            <Grid item xs={6} container style={{"width": "100%"}}>
                 Result
             </Grid> 
             <Grid item xs={1} />

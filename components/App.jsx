61b573131bebe4572efc999791da501316ee838a
rahgadda <gaddam.rahul@gmail.com>
2021-03-16 17:32:12 +0000

    Initial Draft

--- components/App.jsx
+++ components/App.jsx
@@ -1,22 +1,20 @@
 function App() {
     return (
-        <div>
-            <Grid container direction="column" spacing={2}>
-                <Grid item container>
-                    <Header />
-                </Grid>
-                <Grid item container>
-                    <Grid item xs={0} sm={2} />
-                    <Grid item xs={12} sm={8} container> 
-                        <Body />
-                    </Grid>
-                    <Grid item xs={0} sm={2} />
-                </Grid>
-                <Grid item container style={{bottom: 0, "margin-top": "calc(5% + 100px)"}}>
-                    <Footer />
-                </Grid>
+        <Grid container direction="column" spacing={2} xs={12} style={{"max-width": "100%", "max-height":"100%"}}>
+            <Grid item container xs={2} style={{"max-width": "100%", "max-height":"100%"}} >
+                <Header />
             </Grid>
-        </div>
+            <Grid item container xs={8} style={{"max-width": "100%", "max-height":"100%"}}>
+                <Grid item xs={0} sm={2} />
+                <Grid item xs={12} sm={8} container> 
+                    <Body />
+                </Grid>
+                <Grid item xs={0} sm={2} />
+            </Grid>
+            <Grid item container xs={2} style={{"max-width": "100%", "max-height":"100%"}}>
+                <Footer />
+            </Grid>
+        </Grid>
     );
 }
 
--- components/Body.jsx
+++ components/Body.jsx
@@ -1,12 +1,12 @@
 function Body(){
     return (
-        <Grid container direction="column" spacing={2} alignItems="stretch">
+        <Grid container direction="column" spacing={2}>
             <Grid item xs={1} /> 
-            <Grid item xs={2}>
+            <Grid item xs={2} container style={{"max-width": "100%"}}>
                 Search Box
             </Grid> 
             <Grid item xs={2} /> 
-            <Grid item xs={6}>
+            <Grid item xs={6} container style={{"max-width": "100%"}}>
                 Result
             </Grid> 
             <Grid item xs={1} />

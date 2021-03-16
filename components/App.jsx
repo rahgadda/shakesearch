33f182d35320913e9226983b5809a9446c6366d8
rahgadda <gaddam.rahul@gmail.com>
2021-03-16 10:54:14 +0000

    Initial Draft

--- components/App.jsx
+++ components/App.jsx
@@ -1,8 +1,39 @@
+const {
+    colors,
+    CssBaseline,
+    ThemeProvider,
+    Typography,
+    Container,
+    makeStyles,
+    createMuiTheme,
+    Box,
+    SvgIcon,
+    Link,
+} = MaterialUI;
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://rahgadda.github.io/shakesearch/">
+                Rahul Kiran Gaddam
+      </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
 function App() {
     return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
+        <Container maxWidth="sm">
+            <div style={{ marginTop: 24, }}>
+                <Typography variant="h4" component="h1" gutterBottom>
+                    HelloWorld
+                </Typography>
+                <Copyright />
+            </div>
+        </Container>
     )
 }
 

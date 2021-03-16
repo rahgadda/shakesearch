04e488da664a1416136e618582a885b9f536e4b1
rahgadda <gaddam.rahul@gmail.com>
2021-03-16 17:45:26 +0000

    Initial Draft

--- components/App.jsx
+++ components/App.jsx
@@ -1,17 +1,17 @@
 function App() {
     return (
         <Grid container direction="column" spacing={2}>
-            <Grid item container xs={2} style={{"max-width": "100%", "max-height":"100%"}} >
+            <Grid item container xs={2} style={{"maxWidth": "100%", "maxHeight":"100%"}} >
                 <Header />
             </Grid>
-            <Grid item container xs={8} style={{"max-width": "100%", "max-height":"100%"}}>
+            <Grid item container xs={8} style={{"maxWidth": "100%", "maxHeight":"100%"}}>
                 <Grid item xs={1} sm={2} />
                 <Grid item xs={10} sm={8} container> 
                     <Body />
                 </Grid>
                 <Grid item xs={1} sm={2} />
             </Grid>
-            <Grid item container xs={2} style={{"max-width": "100%", "max-height":"100%"}}>
+            <Grid item container xs={2} style={{"maxWidth": "100%", "maxHeight":"100%"}}>
                 <Footer />
             </Grid>
         </Grid>

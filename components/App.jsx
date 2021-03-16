09c6587b5c4c5eb115d12df3609dc9a597885211
rahgadda <gaddam.rahul@gmail.com>
2021-03-16 11:14:19 +0000

    Initial Draft

--- components/App.jsx
+++ components/App.jsx
@@ -1,10 +1,16 @@
-const { Container } = MaterialUI;
-
 function App() {
     return (
-        <Container maxWidth="sm">
-            <div>HelloWorld</div>
-        </Container>
+        <div>
+            <script src="https://unpkg.com/@material-ui/core@latest/umd/material-ui.development.js"  crossorigin="anonymous"></script>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+            
+            const {Container} = MaterialUI;
+
+            <Container maxWidth="sm">
+                <div>HelloWorld</div>
+            </Container>
+        </div>
     )
 }
 
